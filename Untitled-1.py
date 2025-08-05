41de42369780f873c032c7e30c8db979ab3cf0b8
FranciscoC <franciscolcpereira99@gmail.com>
2025-08-05 09:40:36 +0100

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -7,3 +7,11 @@ app = Flask(__name__)
 app.secret_key = 'chave_secret_key'
 app.vonfig['DATABASE'] = 'apex_status.db'
 app.config['API_KEY'] = 'sua_chave_api_aqui'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(app.config['DATABASE'])
+        db.row_factory = sqlite3.Row
+    return db
+
