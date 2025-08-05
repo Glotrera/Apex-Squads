09dab3ad9a182360c4459744ff7dc793daec9cc9
FranciscoC <franciscolcpereira99@gmail.com>
2025-08-05 09:36:27 +0100

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,4 +3,7 @@ import sqlite3
 import requests
 from flask import Flask, render_template, request, redirect, url_for, g, session
 
-app =
+app = Flask(__name__)
+app.secret_key = 'chave_secret_key'
+app.vonfig['DATABASE'] = 'apex_status.db'
+app.config['API_KEY'] = 'sua_chave_api_aqui'
